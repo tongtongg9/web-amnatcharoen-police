1254783daceb70a1bf3a0273bba14293e93f63f5
tongtongg9 <kampon.lamyai@gmail.com>
2023-01-24 14:25:49 +0700

    updated

---
+++ ผู้บังคับบัญชา/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // Create a route object
+    const route = {
+        path: '',
+        method: 'GET',
+        handler: function () {
+            // Handler code here
+            $('#nav-commander').removeClass()
+            $('#nav-commander').addClass('rounded-lg px-3 py-2 border border-white border-transparent hover:bg-primary hover:scale-105 transition active:bg-primary-100')
+        },
+    }
+
+    // Add the route to the router using jQuery's .on() method
+    $(window).on(route.method, route.path, route.handler)
+
+    // Trigger the route when the page loads
+    $(window).trigger(route.method, [route.path])
+})
---
+++ หน่วยในสังกัด/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Create a route object
+    const route = {
+        path: '',
+        method: 'GET',
+        handler: function () {
+            // Handler code here
+            $('#nav-unit').removeClass()
+            $('#nav-unit').addClass(
+                'rounded-lg px-3 py-2 border border-white border-transparent hover:bg-primary hover:scale-105 transition active:bg-primary-100'
+            )
+        },
+    }
+
+    // Add the route to the router using jQuery's .on() method
+    $(window).on(route.method, route.path, route.handler)
+
+    // Trigger the route when the page loads
+    $(window).trigger(route.method, [route.path])
+})
